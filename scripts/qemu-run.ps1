43bfe98d1607cd0dbbd601c57163cb6ad478b4a9
zsy-arch <helloworld1915@outlook.com>
2022-07-16 00:27:09 +0800

    qemu-run

---
+++ scripts/qemu-run.ps1
@@ -0,0 +1,1 @@
+qemu-system-i386.exe -m 1024M -serial stdio -vga virtio -drive file=./Kernel.bin,format=raw
--- source/boot.asm
+++ source/boot.asm
@@ -1,5 +1,6 @@
-org 0x7C00
+org 7C00h
 bits 16
+; boot.bin 由 boot.asm 汇编得到. 负责加载 sysinit.bin 到 0x40000
 start:
 	cli
 	xor ax, ax
@@ -8,8 +9,26 @@ start:
 	sti
 	mov si, welcome_string
 	call k_puts
+load_sysinit:
+	mov dx, 0h
+	mov cx, 0002h
+	mov bx, 0200h
+	mov ax, 0201h
+	int 13h
+	jnc ok_load_sysinit
+	mov dx, 0h
+	mov ax, 0h
+	int 13h
+	mov si, load_init_retry
+	call k_puts
+	jmp load_sysinit
+ok_load_sysinit:
+	mov si, load_init_ok1
+	call k_puts
 k_loop:
 	jmp k_loop
+
+
 k_puts:
 	mov ah, 0Eh
 .repeat:
@@ -23,5 +42,9 @@ k_puts:
 data:
 welcome_string:
 	db "Loading ZSY-ARCH OS(v0.0.1)...", 0Ah, 0Dh, 0h
+load_init_ok1:
+	db "Loading SysInit Module...", 0Ah, 0Dh, 0h
+load_init_retry:
+	db "Loading SysInit Module(Retry)...", 0Ah, 0Dh, 0h
 times 510 - ($ - $$) db 0
 dw 0xAA55
